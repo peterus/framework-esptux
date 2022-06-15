324541a22fb31c9d68c22ed35083e2e1eaba2808
Peter Buchegger <peterus@users.noreply.github.com>
2022-06-15 21:03:33 +0000

    format example

--- example/test.cpp
+++ example/test.cpp
@@ -9,29 +9,29 @@
 static SPISettings spiSettings(4000000, MSBFIRST, SPI_MODE0);
 
 void setup(void) {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    Serial.println("Starting Portduino test");
+  Serial.println("Starting Portduino test");
 
-    pinMode(TEST_PIN, OUTPUT);
-    digitalWrite(TEST_PIN, LOW);
+  pinMode(TEST_PIN, OUTPUT);
+  digitalWrite(TEST_PIN, LOW);
 
-    SPI.begin();
+  SPI.begin();
 
-    SPI.beginTransaction(spiSettings);
-    uint8_t testBuf[4];
-    SPI.transfer(testBuf, sizeof(testBuf));
-    SPI.endTransaction();
+  SPI.beginTransaction(spiSettings);
+  uint8_t testBuf[4];
+  SPI.transfer(testBuf, sizeof(testBuf));
+  SPI.endTransaction();
 
-    Wire.begin();
+  Wire.begin();
 
-    Wire.beginTransmission(0x55);
-    Wire.write(5);
-    Wire.endTransmission();
+  Wire.beginTransmission(0x55);
+  Wire.write(5);
+  Wire.endTransmission();
 
-    Serial.println("Done with setup");
+  Serial.println("Done with setup");
 }
 
 void loop(void) {
-    digitalWrite(TEST_PIN, digitalRead(TEST_PIN) == LOW ? HIGH : LOW);
+  digitalWrite(TEST_PIN, digitalRead(TEST_PIN) == LOW ? HIGH : LOW);
 }
